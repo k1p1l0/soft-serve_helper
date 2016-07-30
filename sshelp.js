75ddb54b65bc2ca8fb79f79a964754b099cfbe3f
k1p1l0 <s0ht@mail.ru>
2016-07-30 17:10:35 +0300

    delete template

--- sshelp.js
+++ sshelp.js
@@ -1,30 +1,10 @@
 ( function (window) {
 
-	function constructor () {
+	function Constructor () {
 		var module = {};
 		
 		module.log = console.log.bind(console),
 		module.dir = console.dir.bind(console);
-
-		module.template = function (tpl, data) {
-			for (let key in data) {
-				if (typeof data[key] === 'object') {
-					var info = [];
-
-					for (let sKey in data[key]) {
-						info.push(data[key][sKey]);
-					}
-
-					tpl = tpl.replace(':' + key, info);
-
-					continue;
-				}
-				data[key] = this.valid(data[key]);
-				tpl = tpl.replace(':' + key, data[key]);
-			}
-
-			return tpl;
-		};
 
 		module.valid = function (string) {
 			if (string <= 0) {
@@ -32,7 +12,7 @@
 			}
 
 			return string;
-		}
+		};
 
 		module.extend = function (Parent, Child) {
 			var Surrogat = function(){};
@@ -41,12 +21,11 @@
 			Child.prototype = new Surrogat();
 			Child.prototype.constuctor = Child;
 			Child.prototype.super = Parent.prototype;
-		}
+		};
 
 		return module;
 	}
 
 	window.SS = constructor();
 
-})(window);
-
+})(typeof window !== "undefined" ? window : this);
