a1a167dd1924bce3a6646603af432925993aef5c
k1p1l0 <s0ht@mail.ru>
2016-07-30 17:00:55 +0300

    fix uglify bug

--- sshelp.js
+++ sshelp.js
@@ -1,55 +1,52 @@
-window.addEventListener('DOMContentLoaded', init, false);
+( function (window) {
 
-function init () {
-	( function (window) {
+	function constructor () {
+		var module = {};
+		
+		module.log = console.log.bind(console),
+		module.dir = console.dir.bind(console);
 
-		function constructor () {
-			var module = {};
-			
-			module.log = console.log.bind(console),
-			module.dir = console.dir.bind(console);
+		module.template = function (tpl, data) {
+			for (let key in data) {
+				if (typeof data[key] === 'object') {
+					var info = [];
 
-			module.template = function (tpl, data) {
-				for (let key in data) {
-					if (typeof data[key] === 'object') {
-						var info = [];
-
-						for (let sKey in data[key]) {
-							info.push(data[key][sKey]);
-						}
-
-						tpl = tpl.replace(':' + key, info);
-
-						continue;
+					for (let sKey in data[key]) {
+						info.push(data[key][sKey]);
 					}
-					data[key] = this.valid(data[key]);
-					tpl = tpl.replace(':' + key, data[key]);
+
+					tpl = tpl.replace(':' + key, info);
+
+					continue;
 				}
-
-				return tpl;
-			};
-
-			module.valid = function (string) {
-				if (string <= 0) {
-					string = 'No information';
-				}
-
-				return string;
+				data[key] = this.valid(data[key]);
+				tpl = tpl.replace(':' + key, data[key]);
 			}
 
-			module.extend = function (Parent, Child) {
-				var Surrogat = function(){};
-				Surrogat.prototype = Parent.prototype;
+			return tpl;
+		};
 
-				Child.prototype = new Surrogat();
-				Child.prototype.constuctor = Child;
-				Child.prototype.super = Parent.prototype;
+		module.valid = function (string) {
+			if (string <= 0) {
+				string = 'No information';
 			}
 
-			return module;
+			return string;
 		}
 
-		window.SS = constructor();
+		module.extend = function (Parent, Child) {
+			var Surrogat = function(){};
+			Surrogat.prototype = Parent.prototype;
 
-	})(window);
-}
+			Child.prototype = new Surrogat();
+			Child.prototype.constuctor = Child;
+			Child.prototype.super = Parent.prototype;
+		}
+
+		return module;
+	}
+
+	window.SS = constructor();
+
+})(window);
+
